1821992968845239c2e83d9a642612a8290991f4
jjmuesesq <jjmuesesq@unal.edu.co>
2020-01-26 13:21:35 -0500

    perzonalizacion y ordenacion directa de paginas

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):   
     title = models.CharField(verbose_name='Titulo', max_length=200)
     content = models.TextField(verbose_name='Contenido')
+    order = models.SmallIntegerField(verbose_name='Orden', default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
     class Meta:
         verbose_name = "Página"
         verbose_name_plural = "Páginas"
-        ordering = ["title"]
+        ordering = ['order', 'title']
     
     def __str__(self):
         return self.title
